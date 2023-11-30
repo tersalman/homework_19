a43f793ce9fd91ff27bdb7329c85866a96238ce2
tersalman <mansalmanter@gmail.com>
2023-11-30 10:06:36 +0300

    the library was added and exception have BAD-REQUEST status

--- src/main/java/com/example/homework19/Exceptions/IlligalLetterException.java
+++ src/main/java/com/example/homework19/Exceptions/IlligalLetterException.java
@@ -1,5 +1,9 @@
 package com.example.homework19.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class IlligalLetterException extends RuntimeException{
     public IlligalLetterException(String message) {
         super(message);
